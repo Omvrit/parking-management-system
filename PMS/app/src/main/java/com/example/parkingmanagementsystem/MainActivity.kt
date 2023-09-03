db42fbb955007024f988cb5bb81bb323d3eebc12
Aniwesh <124008915+Aniwesh-kumar@users.noreply.github.com>
2023-09-03 16:06:35 +0530

    PMS App
    Login and Signup Screen along with the Home Page.

---
+++ PMS/app/src/main/java/com/example/parkingmanagementsystem/MainActivity.kt
@@ -0,0 +1,56 @@
+package com.example.parkingmanagementsystem
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import com.example.parkingmanagementsystem.databinding.ActivityMainBinding
+import com.google.android.material.bottomnavigation.BottomNavigationView
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var bottomNavigationView: BottomNavigationView
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+
+        binding.bottomNavigation.setOnItemSelectedListener { menuItem ->
+            when(menuItem.itemId){
+                R.id.bottom_home -> {
+                    replaceFragment(HomeFragment())
+                    true
+                }
+                R.id.bottom_search -> {
+                    replaceFragment(SearchFragment())
+                    true
+                }
+                R.id.bottom_map -> {
+                    replaceFragment(MapFragment())
+                    true
+                }
+                R.id.bottom_booking -> {
+                    replaceFragment(BookingFragment())
+                    true
+                }
+                R.id.bottom_profile -> {
+                    replaceFragment(ProfileFragment())
+                    true
+                }
+                else -> false
+            }
+        }
+        replaceFragment(HomeFragment())
+
+
+
+        /* attach fragments with bottom navigation view
+        1. initialise the bottom nav 2. replace method 3. set on item selected listener 4. initial fragment
+         */
+
+    }
+    private fun replaceFragment(fragment: Fragment){
+        supportFragmentManager.beginTransaction().replace(R.id.frameContainer, fragment).commit()
+    }
+}
